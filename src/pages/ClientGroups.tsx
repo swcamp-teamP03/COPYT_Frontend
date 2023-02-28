15a630201715c033e01ab6cb0ee38aae3941e30f
parkharoi <haha2000ab@gmail.com>
2023-02-28 23:02:57 +0900

    [Feat] : 라우터 연결

---
+++ src/pages/ClientGroups.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ClientGroups = () => {
+  return <div>고객그룹</div>;
+};
+
+export default ClientGroups;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CopyGroups from '../pages/CopyGroups';
+import ClientGroups from '../pages/ClientGroups';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/copies" element={<CopyGroups />} />
+        <Route path="/clients" element={<ClientGroups />} />
       </Routes>
     </BrowserRouter>
   );
