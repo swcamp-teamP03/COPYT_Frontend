a27b0cefa79c6f7f347ea4edfbde37a126ac3073
Dae-une <ko301201@naver.com>
2023-03-03 13:18:25 +0900

    [fix] GNB

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,14 +4,18 @@ import CopyGroups from '../pages/CopyGroups';
 import ClientGroups from '../pages/ClientGroups';
 import React from 'react';
 import GNB from '../components/common/GNB';
+import CreateCopy from '../pages/CreateCopy';
+import GNBRoute from './GNBRoutes';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <GNB />
       <Routes>
-        <Route path="/copies" element={<CopyGroups />} />
-        <Route path="/clients" element={<ClientGroups />} />
+        <Route element={<GNBRoute hasGNB={true} />}>
+          <Route path="/copies" element={<CopyGroups />} />
+          <Route path="/copies/create" element={<CreateCopy />} />
+          <Route path="/clients" element={<ClientGroups />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
