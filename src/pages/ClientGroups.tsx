b05290225a58c4137f4a40888e9b3e836a978ee5
parkharoi <haha2000ab@gmail.com>
2023-03-03 15:26:22 +0900

    [Feat] : 고객그룹 만드는 중

--- src/pages/ClientGroups.tsx
+++ src/pages/ClientGroups.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-// import ClientGroupList from '../components/ClientGrups';
+import ClientGroupList from '../components/ClientGrups';
 
 const ClientGroups = () => {
-  const totalClient = 100; // 임의의 값으로 설정
-  const clientList = [
+  const totalCopy = 100; // 임의의 값으로 설정
+  const copyList = [
     //  임의의 값으로 설정
-    { clientId: 1, createDate: '2022-03-01', like: true, copyName: 'Copy1', tag: 'Tag1' },
-    { clientId: 2, createDate: '2022-03-02', like: false, copyName: 'Copy2', tag: 'Tag2' },
-    { clientId: 3, createDate: '2022-03-03', like: true, copyName: 'Copy3', tag: 'Tag3' },
+    { copyId: 1, createDate: '2022-03-01', like: true, copyName: 'Copy1', tag: 'Tag1' },
+    { copyId: 2, createDate: '2022-03-02', like: false, copyName: 'Copy2', tag: 'Tag2' },
+    { copyId: 3, createDate: '2022-03-03', like: true, copyName: 'Copy3', tag: 'Tag3' },
   ];
 
-  return <div>{/* <ClientGroupList totalCopy={totalCopy} copyList={copyList} /> */}</div>;
+  return <div>{<ClientGroupList totalCopy={totalCopy} copyList={copyList} />}</div>;
 };
 
 export default ClientGroups;
