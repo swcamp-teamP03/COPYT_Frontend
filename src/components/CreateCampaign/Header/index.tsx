0d1c974ac1ab0c1eea98b70d9732557ac5087940
Dae-une <ko301201@naver.com>
2023-03-07 16:48:12 +0900

    [feat] 캠페인생성 헤더 UI

---
+++ src/components/CreateCampaign/Header/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CHEVRON } from '../../../assets/Chevron';
+import { POST_SVG } from '../../../assets/Post';
+import Button from '../../common/Button';
+import PageHeader from '../../common/PageHeader';
+import * as S from './Header.styles';
+
+interface HeaderProps {
+  editMode: boolean;
+}
+
+const Header = () => {
+  const [editMode, setEditMode] = useState(false);
+  const navigate = useNavigate();
+  const handleEditMode = () => {
+    setEditMode((prev) => !prev);
+  };
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <S.Fixed>
+      <S.Flex>
+        <S.LeftChevron onClick={goBack}>{CHEVRON.left}</S.LeftChevron>
+        <S.Flex>
+          {!editMode ? (
+            <>
+              <S.Title>새 켐페인 230228</S.Title>
+              <S.SVG onClick={handleEditMode}>{POST_SVG.edit}</S.SVG>
+            </>
+          ) : (
+            <>
+              <S.TitleInput />
+              <Button buttonColor="black" title="저장" buttonSize="buttonS" />
+            </>
+          )}
+        </S.Flex>
+      </S.Flex>
+      <div>
+        <Button title="캠페인 실행" buttonColor="black" buttonSize="buttonM" />
+      </div>
+    </S.Fixed>
+  );
+};
+
+export default Header;
---
+++ src/pages/CreateCampaign.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/CreateCampaign/Header';
+
+const CreateCampaign = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default CreateCampaign;
