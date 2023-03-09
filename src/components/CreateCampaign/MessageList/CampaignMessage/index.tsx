657740d2b1285c3ee8d91b496febd6dd970d5295
Dae-une <ko301201@naver.com>
2023-03-09 14:51:41 +0900

    [fix] 메세지 컴포넌트 분리

---
+++ src/components/CreateCampaign/MessageList/CampaignMessage/index.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { POST_SVG } from '../../../../assets/Post';
+import { campaignConditionState } from '../../../../store/campaignConditionState';
+import Button from '../../../common/Button';
+import * as S from './CampaignMessage.styles';
+
+interface CampaignMessageProps {
+  type: 'A' | 'B';
+  member: number;
+  initMessage: string;
+}
+
+const CampaignMessage = ({ type, member, initMessage }: CampaignMessageProps) => {
+  const [condition, setCondition] = useRecoilState(campaignConditionState);
+  const [message, setMessage] = useState(initMessage);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleEditMode = () => {
+    setEditMode((prev) => !prev);
+  };
+  const onChangeMessage = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(event.target.value);
+  };
+
+  const onSaveMessage = (type: 'A' | 'B') => {
+    setCondition((prev) => ({
+      ...prev,
+      [`message${type}`]: message,
+    }));
+    handleEditMode();
+  };
+
+  const maxByte = condition.messageType === 'SMS' ? 140 : 20000;
+
+  useEffect(() => {
+    setMessage(initMessage);
+  }, [initMessage]);
+
+  return (
+    <S.MessageLayout>
+      <S.MessageMember>
+        <span>메시지 {type}</span>
+        <span>{member}명</span>
+      </S.MessageMember>
+      <S.MessageContainer>
+        <S.MessageHeader>
+          <div>
+            <div>[Web발신]</div>
+            {condition.sentType === 'AD' && <span>(광고)</span>}
+          </div>
+          {!editMode && <S.EditIcon onClick={handleEditMode}>{POST_SVG.edit}</S.EditIcon>}
+        </S.MessageHeader>
+        <S.MessageBody>{editMode ? <S.EditText value={message} onChange={onChangeMessage} name="message" /> : <div>{condition[`message${type}`]}</div>}</S.MessageBody>
+        <S.MessageFooter>
+          <span>copyt.bz/LABzD</span>
+          <div>[무료수신거부]</div>
+          <span>copyt.li/ABCDEFGH</span>
+          <S.Byte>
+            {message.length * 2} /{maxByte}
+          </S.Byte>
+        </S.MessageFooter>
+      </S.MessageContainer>
+      {editMode && (
+        <S.EditButtons>
+          <Button title="취소" buttonColor="white" buttonSize="buttonS" onButtonClick={handleEditMode} />
+          <Button title="저장" buttonColor="black" buttonSize="buttonS" onButtonClick={() => onSaveMessage(type)} />
+        </S.EditButtons>
+      )}
+    </S.MessageLayout>
+  );
+};
+
+export default CampaignMessage;
