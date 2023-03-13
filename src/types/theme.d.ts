3ea896022a7bf8d8f71dfe309e3c3d1747678b90
김대은 <ko301201@naver.com>
2023-03-13 22:50:50 +0900

    [fix] theme.d.ts

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,17 +1,10 @@
 import 'styled-components';
-<<<<<<< .merge_file_6pncjg
 import { ButtonsType, ColorsType, FontsType } from '../styles/theme';
-=======
-import { ColorsType, FontsType } from '../styles/theme';
->>>>>>> .merge_file_HmICiu
 
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: ColorsType;
     fontSize: FontsType;
-<<<<<<< .merge_file_6pncjg
     button: ButtonsType;
-=======
->>>>>>> .merge_file_HmICiu
   }
 }
