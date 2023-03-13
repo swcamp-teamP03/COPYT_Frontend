3bf4eb39e4744f09df034e84e424c7add3c9291a
Dae-une <ko301201@naver.com>
2023-03-13 15:25:58 +0900

    [fix] 버튼 호버시 색상 변경

--- src/components/common/Button/Button.style.ts
+++ src/components/common/Button/Button.style.ts
@@ -11,15 +11,15 @@ export const HeaderButton = styled.button<ButtonProps>`
   ${(props) => props.theme.button[`${props.buttonSize}`]}
   cursor: pointer;
   background-color: ${(props) => (props.buttonColor === 'blue' ? props.theme.colors.blue30 : props.theme.colors.white)};
-  color: ${(props) => (props.buttonColor === 'blue' ? props.theme.colors.white : 'black')};
+  color: ${(props) => (props.buttonColor === 'blue' ? props.theme.colors.white : props.theme.colors.blue30)};
   border: ${(props) => (props.buttonColor === 'white' ? 'solid 1px' : 'none')};
   border-radius: ${(props) => props.borderRadius};
   :hover {
-    background-color: ${(props) => (props.buttonColor === 'blue' ? props.theme.colors.blue40 : props.theme.colors.gray20)};
+    background-color: ${(props) => (props.buttonColor === 'white' ? props.theme.colors.blue20 : props.theme.colors.blue40)};
   }
   :disabled {
     cursor: default;
-    background-color: ${({ theme, buttonColor }) => (buttonColor === 'blue' ? theme.colors.gray10 : theme.colors.white)};
+    background-color: ${({ theme, buttonColor }) => (buttonColor === 'blue' ? theme.colors.gray40 : theme.colors.white)};
     color: ${({ theme, buttonColor }) => (buttonColor === 'blue' ? theme.colors.white : theme.colors.gray40)};
   }
 `;
