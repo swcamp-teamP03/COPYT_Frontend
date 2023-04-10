9f110438b173db9c6a2cda08a5623db875af641e
김대은 <104764474+Dae-une@users.noreply.github.com>
2023-04-10 23:58:46 +0900

    [Fix] 카피 생성 컨디션 변경 및 상세 내역 디자인 변경 (#86) (#87)
    * [fix] 카피 생성 타이틀 변경
    
    * [fix] 핵심 키워드 설명 추가
    
    * [fix] 카피 생성 컨디션 수정
    
    * [fix]카피 추천 리스트 없을 경우 이미지 변경
    
    * [fix] 카피 상세내역 변경

--- src/components/common/LabelInput/index.tsx
+++ src/components/common/LabelInput/index.tsx
@@ -1,5 +1,7 @@
 import React, { InputHTMLAttributes, useRef } from 'react';
+import { SVG } from '../../../assets';
 import { VALIDATE } from '../../../assets/Validate';
+import HoverQuestion from '../../DetailCampaign/Analysis/HoverQuestion';
 import * as S from './LabelInput.styles';
 
 interface LabelInputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -8,9 +10,11 @@ interface LabelInputProps extends InputHTMLAttributes<HTMLInputElement> {
   isRequire?: boolean;
   errorMessage?: string;
   limit?: number;
+  desc?: string;
+  hover?: string;
 }
 
-const LabelInput = ({ labelTitle, flexDirection = 'column', isRequire = true, errorMessage, limit, ...props }: LabelInputProps) => {
+const LabelInput = ({ labelTitle, flexDirection = 'column', isRequire = true, errorMessage, desc, limit, hover, ...props }: LabelInputProps) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const textCount = inputRef.current?.value.length ?? 0;
@@ -19,10 +23,17 @@ const LabelInput = ({ labelTitle, flexDirection = 'column', isRequire = true, er
       <S.Label>
         {labelTitle}
         {isRequire && <span>*</span>}
+        {hover && <HoverQuestion text={hover} left={'-25px'} />}
       </S.Label>
       <S.Input {...props} ref={inputRef} />
+      {desc && (
+        <S.Desc>
+          {SVG.exclamation}
+          <span>{desc}</span>
+        </S.Desc>
+      )}
       {limit && (
-        <S.TextCount>
+        <S.TextCount hasDesc={desc ? true : false}>
           {textCount}/ {limit}
         </S.TextCount>
       )}
