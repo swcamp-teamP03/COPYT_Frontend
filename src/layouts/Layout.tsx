09d296b3dc2662fc6cee33f775ee24b61af73d07
parkharoi <haha2000ab@gmail.com>
2023-04-18 01:47:19 +0900

    [ref] home ui 수정

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
 import GNB from '../components/common/GNB';
 
 interface LayoutProps {
@@ -8,12 +9,15 @@ interface LayoutProps {
 }
 
 const Layout = ({ router }: LayoutProps) => {
+  const location = useLocation();
+  const isRoot = location.pathname === '/main';
+
   return (
     <Wrapper>
       <Sticky>
         <GNB />
       </Sticky>
-      <ContentWrapper>
+      <ContentWrapper isRoot={isRoot}>
         <Content>{router}</Content>
       </ContentWrapper>
     </Wrapper>
@@ -30,11 +34,12 @@ const Sticky = styled.div`
   top: 0;
 `;
 
-const ContentWrapper = styled.div`
+const ContentWrapper = styled.div<{ isRoot: boolean }>`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
   padding: 4.25rem 4.25rem;
+  background-color: ${({ isRoot }) => (isRoot ? '#f2f2f2' : 'transparent')};
 `;
 
 const Content = styled.div`
